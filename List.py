acc508e8896b232a307db24b0e4178d21ecf599a
WIJAYA-MCS <90946868+WIJAYA-MCS@users.noreply.github.com>
2022-01-22 13:09:37 +0700

    Add files via upload

---
+++ List.py
@@ -0,0 +1,14 @@
+my_list = [1, 'Jeruk', True, 7.5]
+print(my_list)
+
+a = [1,2,3,4]
+print(a[1:3])
+print(a[-2])
+
+fruits = ["Mangga", "Apple", "Durian"] # cek append dan insert
+fruits.append("Jeruk")
+print(fruits)
+
+fruits.insert(1, "Jeruk")
+print(fruits)
+
